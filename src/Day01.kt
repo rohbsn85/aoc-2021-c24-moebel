9096e878b217ad3cf869ac4d472ba35ca46aec20
Bjoern Kinberger <bjoern.kinberger@check24.de>
2021-12-01 11:23:24 +0100

    we fix it!

--- src/Day01.kt
+++ src/Day01.kt
@@ -11,7 +11,36 @@ fun main() {
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01").map {
+        it.toInt()
+    }
+    var forEachCounter = 0
+    input.forEachIndexed { index, _ ->
+        if (index != 0) {
+            if (input[index] > input[index - 1]) {
+                forEachCounter++
+            }
+        }
+    }
+    print("forEach: $forEachCounter")
+
+
+    val foldCounter = input.fold(Day01(input[0], 0)) { acc, value ->
+        acc.copy(previousValue = value, sum = if (value > acc.previousValue) acc.sum + 1 else acc.sum)
+    }
+    print("\n")
+    print("fold: ${foldCounter.sum}")
+
+    var prevValue = Int.MAX_VALUE
+    val count =
+        input.count {
+            val value = it > prevValue
+            prevValue = it
+            value
+        }
+
+    print("\n")
+    print(count)
 }
+
+data class Day01(val previousValue: Int, val sum: Int)
