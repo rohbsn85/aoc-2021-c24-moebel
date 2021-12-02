a5d2d68b0638c93c71b53ca32b753cc1e63e0672
Robert Kerz <robert.kerz@check24.de>
2021-12-02 16:21:31 +0100

    add day2 part1 solution

--- src/day_02/solution/Day02.kt
+++ src/day_02/solution/Day02.kt
@@ -1,2 +1,63 @@
 package day_02.solution
 
+import readInput
+
+fun main() {
+
+    val input = readInput("day_02/data/Day02_rob").mapNotNull { line ->
+        val splitted = line.split(" ")
+
+        if (splitted.size == 2) {
+            val direction = Direction.fromKey(splitted[0])
+            val distance = splitted[1].toInt()
+
+            direction?.let {
+                Command(it, distance)
+            }
+        } else {
+            null
+        }
+    } // [Command(forward, 9), .... ]
+
+    val position = input.fold(SubmarinePosition()) { currentPosition, command ->
+        command.direction.driveAction(currentPosition, command.distance)
+    }
+
+    print(position.multiplied())
+}
+
+enum class Direction(val key: String, val driveAction: (SubmarinePosition, Int) -> SubmarinePosition) {
+    FORWARD("forward", { currentPosition, distance ->
+        currentPosition.copy(
+            horizontal = currentPosition.horizontal + distance
+        )
+    }),
+    DOWN("down", { currentPosition, distance ->
+        currentPosition.copy(
+            depth = currentPosition.depth + distance
+        )
+    }),
+    UP("up", { currentPosition, distance ->
+        currentPosition.copy(
+            depth = currentPosition.depth - distance
+        )
+    }),
+//    BACKWARDS("backwards"),
+    ;
+
+    companion object {
+        fun fromKey(key: String) = values().firstOrNull { it.key == key }
+    }
+}
+
+data class Command(
+    val direction: Direction,
+    val distance: Int,
+)
+
+data class SubmarinePosition(
+    var horizontal: Int = 0,
+    var depth: Int = 0,
+) {
+    fun multiplied() = horizontal * depth
+}
